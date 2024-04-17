12aa3d5f6be3d6136119cafc9754c90d677f16ef
ArtNov90 <art.novara@gmail.com>
2024-04-17 17:35:33 +0200

    TP EXCEPTIONS travail réaliser: si l’utilisateur saisit une lettre au lieu de chiffres pour le min

--- src/main/java/fr/diginamic/recensement/services/RecherchePopulationBorneService.java
+++ src/main/java/fr/diginamic/recensement/services/RecherchePopulationBorneService.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 import fr.diginamic.recensement.entites.Recensement;
 import fr.diginamic.recensement.entites.Ville;
+import fr.diginamic.recensement.exceptions.SaisieException;
 
 /**
  * Recherche et affichage de toutes les villes d'un département dont la
@@ -16,8 +17,8 @@ import fr.diginamic.recensement.entites.Ville;
  */
 public class RecherchePopulationBorneService extends MenuService {
 
-	@Override
-	public void traiter(Recensement rec, Scanner scanner) {
+	
+	public void traiter(Recensement rec, Scanner scanner) throws SaisieException {
 
 		System.out.println("Quel est le code du département recherché ? ");
 		String choix = scanner.nextLine();
@@ -25,12 +26,17 @@ public class RecherchePopulationBorneService extends MenuService {
 		System.out.println("Choississez une population minimum (en milliers d'habitants): ");
 		String saisieMin = scanner.nextLine();
 		
+		//Si la saisi est n'est pas un nombre , affiche un message d'erreur
+		if (!isInteger(saisieMin)) {
+			throw new SaisieException("Veuillez saisir un nombre pour la population minimale.");
+		}
+
 		System.out.println("Choississez une population maximum (en milliers d'habitants): ");
 		String saisieMax = scanner.nextLine();
 
 		int min = Integer.parseInt(saisieMin) * 1000;
 		int max = Integer.parseInt(saisieMax) * 1000;
-		
+
 		List<Ville> villes = rec.getVilles();
 		for (Ville ville : villes) {
 			if (ville.getCodeDepartement().equalsIgnoreCase(choix)) {
@@ -40,5 +46,15 @@ public class RecherchePopulationBorneService extends MenuService {
 			}
 		}
 	}
-
+	
+	private boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
+
+
